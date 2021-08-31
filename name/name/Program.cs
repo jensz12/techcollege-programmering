11c50c82f20d87d0dc25297cfd63f7c213780374
Jens <jensz123@gmail.com>
2021-08-31 10:44:21 +0200

    Name

---
+++ name/name/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] fornavne = { "Ib", "Kim", "Helle", "Jens", "Ove" };
+            foreach (string skuffe in fornavne)
+            {
+                Console.WriteLine(skuffe);
+            }
+            Console.ReadLine();
+            string[] efternavne = { "Jensen", "Nielsen", "Larsen" };
+            foreach (string kasse in efternavne)
+            {
+                //Console.WriteLine(kasse);
+                foreach (string skuffe in fornavne)
+                {
+                    Console.WriteLine(skuffe + " " + kasse);
+                }
+            }
+            Console.WriteLine("Indtast 3 efternavn");
+            efternavne[0] = Console.ReadLine();
+            efternavne[1] = Console.ReadLine();
+            efternavne[2] = Console.ReadLine();
+            /*
+                foreach (string skuffe in efternavne)
+            {
+                Console.WriteLine(skuffe);
+            }
+            */
+            foreach (string kasse in efternavne)
+            {
+                //Console.WriteLine(kasse);
+                foreach (string skuffe in fornavne)
+                {
+                    Console.WriteLine(skuffe + " " + kasse);
+                }
+            }
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+                }
+           }
+}
